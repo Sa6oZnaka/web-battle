4d468a422d69322a6e938c9a6f235303f24ae15e
Alexadnar Karapenev <treildr@gmail.com>
2019-07-15 13:13:59 +0300

    Added P5JS

---
+++ public/GameMap.js
@@ -0,0 +1,12 @@
+export class GameMap {
+
+    constructor(sizeX, sizeY){
+        this.map = GameMap.init(sizeX, sizeY);
+    }
+
+    static init(){
+        return 0;
+    }
+
+}
+
---
+++ public/Main.js
@@ -0,0 +1,14 @@
+import {GameMap} from "./GameMap.js";
+
+function setup() {
+    let gameMap = new GameMap(10, 10);
+    console.log(gameMap.map);
+}
+
+function draw() {
+    background(55);
+    ellipse(50, 50, 80, 80);
+}
+
+window.setup = setup;
+window.draw = draw;
