3eed2a6bd5cad28fe0b9720e49ac5138bbd7951d
Alexadnar Karapenev <treildr@gmail.com>
2019-09-05 09:56:13 +0300

    Added getNeighbours method

--- public/api/GameMap.js
+++ public/api/GameMap.js
@@ -39,5 +39,28 @@ export class GameMap {
         }
     }
 
+    outOfBonds(x, y) {
+        return x < 0 || y < 0 || x > this.map[0].length || y > this.map.length;
+    }
+
+    getNeighbors(x, y){
+        let neighbors = [];
+
+        if(x % 2 === 0){
+            if(! this.outOfBonds(x + 1, y - 1))neighbors.push(this.map[y - 1][x + 1]);
+            if(! this.outOfBonds(x - 1, y - 1))neighbors.push(this.map[y - 1][x - 1]);
+        }else{
+            if(! this.outOfBonds(x + 1, y + 1))neighbors.push(this.map[y + 1][x + 1]);
+            if(! this.outOfBonds(x - 1, y + 1))neighbors.push(this.map[y + 1][x - 1]);
+        }
+
+        if(! this.outOfBonds(x, y - 1))neighbors.push(this.map[y - 1][x]);
+        if(! this.outOfBonds(x, y + 1))neighbors.push(this.map[y + 1][x]);
+        if(! this.outOfBonds(x - 1, y))neighbors.push(this.map[y][x - 1]);
+        if(! this.outOfBonds(x + 1, y))neighbors.push(this.map[y][x + 1]);
+
+        return neighbors;
+    }
+
 }
 
